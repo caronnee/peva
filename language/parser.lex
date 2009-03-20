40cb82b13a49f7adb2a11f0324403c4a96b42b26
caronnee <caronnee@11875ef8-eda2-11dd-ada0-abde575de245>
2009-03-20 20:39:01 +0000

    opraveno par veci vo flexe

--- language/parser.lex
+++ language/parser.lex
@@ -4,92 +4,59 @@
 %option 8bit
 %option nounput
 
+%x COMMENT_LINE
+%x COMMENT
 %{
 	#include "tokens.h"
 	#include "header1.h"
 %}
 
-DIGIT			[0-9]
 WHITESPACE		[ \r\n\t\f]
 
 %%
 
-{DIGIT}*			printf("cislo\n");
-
 {WHITESPACE}+	printf("whitespace!");	
-\<		return TOKEN_LT;
-\<\= 	return TOKEN_LE;
-\!\= 	return TOKEN_NE;
-\>\= 	return TOKEN_GE;
-\> 	return TOKEN_GT;
 
-\+|\-	return TOKENGE_OPER_SIGNADD;
-
-\*	return TOKENGE_ASTERISK;
-\/ 	return TOKENGE_SOLIDUS;
-div 	return TOKENGE_DIV;
-mod 	return TOKENGE_MOD;
-and 	return TOKENGE_AND;
-
-/* keywords */
 point 	return TOKEN_POINT;
 var 	return TOKEN_VAR;
 function 	return TOKEN_FUNCTION;
 array 	return TOKEN_ARRAY;
-return return TOKEN_RETURN
-if	return TOKEN_IF;		/* if */
 else 	return TOKEN_ELSE;
 while 	return TOKEN_WHILE;
 do	return TOKEN_DO;		/* do */
 repeat 	return TOKEN_REPEAT;
 until 	return TOKEN_UNTIL;
 for 	return TOKEN_FOR;
-or	return TOKEN_OR;		/* or */
+\|\|	return TOKEN_OR;		/* or */
+if	return TOKEN_IF;		/* if */
 not 	return TOKEN_NOT;
-
-/* bot features */
-move	return TOKEN_MOVE;
-run	return TOKEN_RUN;
-shoot	return TOKEN_SHOOT;
-wait	return TOKEN_WAIT;
-walk	return TOKEN_WALK;
-see	return TOKEN_SEE;
-
-/* object features */
-isPlayer	return TOKEN_ISPLAYER;
-isMoving	return TOKEN_ISMOVING;
-isWall		return TOKEN_ISWALL;
-isHit		return TOKEN_ISHIT;
-Locate		return TOKEN_LOCATION;
-Distance	return TOKEN_DISTANCE;
-\.x 		return TOKEN_X;
-\.y		return TOKEN_Y;
-
-
-/* literals */
-[A-Za-z]+[A-Z_a-z0-9] 	return TOKEN_IDENTIFIER;
-[0-9]+		 	return TOKEN_UINT;
-[0-9]+(\.[0-9]+)?([Ee][\+\-]?[0-9]+)?	return TOKEN_REAL;
-string 	return TOKEN_STRING;
-
-/* delimiters */
-
+return return TOKEN_RETURN;
 \; 	return TOKEN_SEMICOLON;
-\. 	return TOKEN_DOT;
 \, 	return TOKEN_COMMA;
 \=\= 	return TOKEN_EQ;		
 \: 	return TOKEN_COLON;
 \( 	return TOKEN_LPAR;
 \) 	return TOKEN_RPAR;
-\[ 	return TOKEN_LSBRA;
+\[	return TOKEN_LSBRA;
 \] 	return TOKEN_RSBRA;
 = 	return TOKEN_ASSIGN;
-
-/* group tokens */
+run|shoot|wait|walk|see	return TOKEN_BOT;
+[A-Za-z]+[A-Z_a-z0-9] 	return TOKEN_IDENTIFIER;
+[0-9]+		 	return TOKEN_UINT;
+[0-9]+(\.[0-9]+)?([Ee][\+\-]?[0-9]+)?	return TOKEN_REAL;
 \<|\>|\<=|\>=|!= 	return TOKEN_OPER_REL;
 \+|\- 	return TOKEN_OPER_SIGNADD;
-\*\/	return TOKEN_OPER_MUL;
-
+\*|\/|\%|&&	return TOKEN_OPER_MUL;
+\/\/		BEGIN(COMMENT_LINE);
+<COMMENT_LINE>
+{
+\n	BEGIN(INITIAL);
+}
+<COMMENT>
+{
+\*\/	BEGIN(INITIAL);
+}
+.	printf("ERROR!\n");
 %%
 
 int main(int argc, char ** argv)
@@ -99,7 +66,7 @@ int main(int argc, char ** argv)
 	int tok;
 	while((tok == yylex())!=0)
 	{
-		printf("mam token %d", tok);
+		printf("mam token %d\n", tok);
 	}
 	return 0;
 }
--- language/tokens.h
+++ language/tokens.h
@@ -9,49 +9,42 @@
 
 /* group elements */
 #define TOKENGE_OPER_REL	10
-#define TOKEN_LT		(DUTOKGE_OPER_REL+1)	    /* < */
-#define TOKEN_LE		(DUTOKGE_OPER_REL+2)	    /* <= */
-#define TOKEN_NE		(DUTOKGE_OPER_REL+3)	    /* != */
-#define TOKEN_GE		(DUTOKGE_OPER_REL+4)	    /* >= */
-#define TOKEN_GT		(DUTOKGE_OPER_REL+5)	    /* > */
+#define TOKEN_LT		(TOKENGE_OPER_REL+1)	    /* < */
+#define TOKEN_LE		(TOKENGE_OPER_REL+2)	    /* <= */
+#define TOKEN_NE		(TOKENGE_OPER_REL+3)	    /* != */
+#define TOKEN_GE		(TOKENGE_OPER_REL+4)	    /* >= */
+#define TOKEN_GT		(TOKENGE_OPER_REL+5)	    /* > */
 
 #define TOKENGE_OPER_SIGNADD	20
-#define TOKENGE_PLUS		(DUTOKGE_OPER_SIGNADD+1)    /* + */
-#define TOKENGE_MINUS		(DUTOKGE_OPER_SIGNADD+2)    /* - */
+#define TOKENGE_PLUS		(TOKENGE_OPER_SIGNADD+1)    /* + */
+#define TOKENGE_MINUS		(TOKENGE_OPER_SIGNADD+2)    /* - */
 
 #define TOKENGE_OPER_MUL	30
-#define TOKENGE_ASTERISK	(DUTOKGE_OPER_MUL+1)	    /* * */
-#define TOKENGE_SOLIDUS		(DUTOKGE_OPER_MUL+2)	    /* / */
-#define TOKENGE_DIV		(DUTOKGE_OPER_MUL+3)	    /* div */
-#define TOKENGE_MOD		(DUTOKGE_OPER_MUL+4)	    /* mod */
-#define TOKENGE_AND		(DUTOKGE_OPER_MUL+5)	    /* and */
+#define TOKENGE_ASTERISK	(TOKENGE_OPER_MUL+1)	    /* * */
+#define TOKENGE_SOLIDUS		(TOKENGE_OPER_MUL+2)	    /* / */
+#define TOKENGE_DIV		(TOKENGE_OPER_MUL+3)	    /* div */
+#define TOKENGE_MOD		(TOKENGE_OPER_MUL+4)	    /* mod */
+#define TOKENGE_AND		(TOKENGE_OPER_MUL+5)	    /* and */
 
 /* keywords */
 #define TOKEN_KEYWORD		100
-#define TOKEN_POINT					    /* point */
-#define TOKEN_VAR		(DUTOK_KEYWORD+5)	    /* var */
-#define TOKEN_FUNCTION		(DUTOK_KEYWORD+9)	    /* function */
-#define TOKEN_ARRAY		(DUTOK_KEYWORD+10)	    /* array */
-#define TOKEN_IF		(DUTOK_KEYWORD+13)	    /* if */
-#define TOKEN_ELSE		(DUTOK_KEYWORD+15)	    /* else */
-#define TOKEN_WHILE		(DUTOK_KEYWORD+16)	    /* while */
-#define TOKEN_DO		(DUTOK_KEYWORD+17)	    /* do */
-#define TOKEN_REPEAT		(DUTOK_KEYWORD+18)	    /* repeat */
-#define TOKEN_UNTIL		(DUTOK_KEYWORD+19)	    /* until */
-#define TOKEN_FOR		(DUTOK_KEYWORD+20)	    /* for */
-#define TOKEN_OR		(DUTOK_KEYWORD+21)	    /* or */
-#define TOKEN_NOT		(DUTOK_KEYWORD+22)	    /* not */
+#define TOKEN_POINT		(TOKEN_KEYWORD+1)	    /* point */
+#define TOKEN_VAR		(TOKEN_KEYWORD+2)	    /* var */
+#define TOKEN_FUNCTION		(TOKEN_KEYWORD+3)	    /* function */
+#define TOKEN_ARRAY		(TOKEN_KEYWORD+4)	    /* array */
+#define TOKEN_IF		(TOKEN_KEYWORD+5)	    /* if */
+#define TOKEN_ELSE		(TOKEN_KEYWORD+6)	    /* else */
+#define TOKEN_WHILE		(TOKEN_KEYWORD+7)	    /* while */
+#define TOKEN_DO		(TOKEN_KEYWORD+8)	    /* do */
+#define TOKEN_REPEAT		(TOKEN_KEYWORD+9)	    /* repeat */
+#define TOKEN_UNTIL		(TOKEN_KEYWORD+10)	    /* until */
+#define TOKEN_FOR		(TOKEN_KEYWORD+11)	    /* for */
+#define TOKEN_OR		(TOKEN_KEYWORD+12)	    /* or */
+#define TOKEN_NOT		(TOKEN_KEYWORD+13)	    /* not */
+#define TOKEN_RETURN		(TOKEN_KEYWORD+14)	   /* return */
+#define TOKEN_BREAK		(TOKEN_KEYWORD+15)	   /* break */
 
-/* bot features */
-#define TOKEN_BOT		200
-#define TOKEN_MOVE		(TOKEN_BOT+1)
-#define TOKEN_RUN   		(TOKEN_NOT+2)
-#define TOKEN_SHOOT             (TOKEN_BOT+3)
-#define TOKEN_WAIT              (TOKEN_BOT+4)
-#define TOKEN_WALK              (TOKEN_BOT+5)
-#define TOKEN_SEE               (TOKEN_BOT+6)
-
-/* object features */
+/* object features 
 #define TOKEN_OBJECT		250
 #define TOKEN_ISPLAYER		(TOKEN_OBJECT+1)
 #define TOKEN_ISMOVING		(TOKEN_OBJECT+2)
@@ -61,32 +54,33 @@
 #define TOKEN_DISTANCE		(TOKEN_OBJECT+6)
 #define TOKEN_X			(TOKEN_OBJECT+7)
 #define TOKEN_Y			(TOKEN_OBJECT+8)
+#define TOKEN_GETID		(TOKEN_OBJECT+9)*/
 
 
 /* literals */
 #define TOKEN_LITERAL		300
-#define TOKEN_IDENTIFIER	(DUTOK_LITERAL+1)	    /* identifier */
-#define TOKEN_UINT		(DUTOK_LITERAL+2)	    /* unsigned integer */
-#define TOKEN_REAL		(DUTOK_LITERAL+3)	    /* real number */
-#define TOKEN_STRING		(DUTOK_LITERAL+4)	    /* string */
+#define TOKEN_IDENTIFIER	(TOKEN_LITERAL+1)	    /* identifier */
+#define TOKEN_UINT		(TOKEN_LITERAL+2)	    /* unsigned integer */
+#define TOKEN_REAL		(TOKEN_LITERAL+3)	    /* real number */
 
 /* delimiters */
 #define TOKEN_DELIMITER		500
-#define TOKEN_SEMICOLON		(DUTOK_DELIMITER+1)	    /* ; */
-#define TOKEN_DOT		(DUTOK_DELIMITER+2)	    /* . */
-#define TOKEN_COMMA		(DUTOK_DELIMITER+3)	    /* , */
-#define TOKEN_EQ		(DUTOK_DELIMITER+4)	    /* == */
-#define TOKEN_COLON		(DUTOK_DELIMITER+5)	    /* : */
-#define TOKEN_LPAR		(DUTOK_DELIMITER+6)	    /* ( */
-#define TOKEN_RPAR		(DUTOK_DELIMITER+7)	    /* ) */
-#define TOKEN_LSBRA		(DUTOK_DELIMITER+9)	    /* [ */
-#define TOKEN_RSBRA		(DUTOK_DELIMITER+10)	    /* ] */
-#define TOKEN_ASSIGN		(DUTOK_DELIMITER+11)	    /* = */
+#define TOKEN_SEMICOLON		(TOKEN_DELIMITER+1)	    /* ; */
+#define TOKEN_DOT		(TOKEN_DELIMITER+2)	    /* . */
+#define TOKEN_COMMA		(TOKEN_DELIMITER+3)	    /* , */
+#define TOKEN_EQ		(TOKEN_DELIMITER+4)	    /* == */
+#define TOKEN_COLON		(TOKEN_DELIMITER+5)	    /* : */
+#define TOKEN_LPAR		(TOKEN_DELIMITER+6)	    /* ( */
+#define TOKEN_RPAR		(TOKEN_DELIMITER+7)	    /* ) */
+#define TOKEN_LSBRA		(TOKEN_DELIMITER+9)	    /* [ */
+#define TOKEN_RSBRA		(TOKEN_DELIMITER+10)	    /* ] */
+#define TOKEN_ASSIGN		(TOKEN_DELIMITER+11)	    /* = */
 
 /* group tokens */
 #define TOKEN_GROUP_TOKEN	700
-#define TOKEN_OPER_REL		(DUTOK_GROUP_TOKEN+1)	    /* < > <= >= <> */
-#define TOKEN_OPER_SIGNADD	(DUTOK_GROUP_TOKEN+2)	    /* + - */
-#define TOKEN_OPER_MUL		(DUTOK_GROUP_TOKEN+3)	    /* * / div mod and */
+#define TOKEN_OPER_REL		(TOKEN_GROUP_TOKEN+1)	    /* < > <= >= <> */
+#define TOKEN_OPER_SIGNADD	(TOKEN_GROUP_TOKEN+2)	    /* + - */
+#define TOKEN_OPER_MUL		(TOKEN_GROUP_TOKEN+3)	    /* * / div mod and */
+#define TOKEN_BOT		(TOKEN_GROUP_TOKEN+4)	   //* bot features */ MOVE,RUN,SHOOT,WAIT,WALK,SEE
 
 #endif
