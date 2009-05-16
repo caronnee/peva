6c35fc69237d0f76624e6301ec5c6e743cffeed0
caronnee <caronnee@11875ef8-eda2-11dd-ada0-abde575de245>
2009-05-16 12:58:02 +0000

    obrazky k codewars

--- new_src/wall.h
+++ new_src/wall.h
@@ -1,28 +1,42 @@
 #include <SDL/SDL.h>
 #include "object.h"
 
-class Wall:public Object //oddelenie urovne abstrakcie:)
+enum WallType
+{
+	SolidWall = 0, //o okolo sa to maposutu doprava pre masku
+	PushableWall,
+	TrapWall,
+	ExitWall;
+	NumberOfWalls;
+}
+class Tile:public Object //oddelenie urovne abstrakcie:)
 {
 public:
-	Wall();
+	Tile();
 	//virtual void damage(Object * sender);
 };
-class SolidWall:public Wall //nic specialneho, proste sten a s nejkou odolnostou
+class SolidWall:public Tile //nic specialneho, proste sten a s nejkou odolnostou
 {
 public:
 	SolidWall();
 	//virtual void damage(Object * sender); 
 };
 
-class PushableWall:public Wall //da sa nou pohnut
+class PushableWall:public Tile //da sa nou pohnut
 {
 public:
 	PushableWall();
 	//virtual void damage(Object * sender);
 };
-class TrapWall:public Wall // opstey na zemi sa vystrkujuce:)
+class TrapWall:public Tile // opstey na zemi sa vystrkujuce:)
 {
 public:
 	TrapWall();
+	//virtual void damage(Object * sender);
+};
+class ExitWall:public Tile // opstey na zemi sa vystrkujuce:)
+{
+public:
+	ExitWall();
 	//virtual void damage(Object * sender);
 };
