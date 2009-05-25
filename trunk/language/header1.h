e9a09daf706d2da88ab95801656019e717c3fb27
caronnee <caronnee@11875ef8-eda2-11dd-ada0-abde575de245>
2009-05-25 13:59:05 +0000

    nejake upravy, stale by to malo byt funkcne

--- trunk/language/header1.h
+++ trunk/language/header1.h
@@ -2,15 +2,47 @@
 #define HEADER_H_
 
 #include <string>
+#include "./tree/my_stack.h"
+
+enum Operation
+{
+	OperationPlus,
+	OperationMinus,
+	OperationMultiply,
+	OperationDivide,
+	OperationModulo,
+
+	OperationLess,
+	OperationLessEqual,
+	OperationGreater,
+	OperationGreaterEqual,
+	OperationEqual,
+	OperationNotEqual,
+
+	OperationAnd,
+	OperationOr,
+	OperationNot,
+
+	OperationBoolAnd,
+	OperationBoolOr,
+	OperationBoolNot
+};
 
 struct Lval {
+	Operation operation;
 	int number;
 	std::string ident; //+mozno este nejake sa njdu
 	float f_number;
+	std::vector<Array> arrays;
+	//block
+	Instructions block;
+	std::vector<Node *> names;
+	bool is_integer;
+	Node * node;
 };
 
 // lex interface
-#define YY_DECL	    int yylex(Lval *lv, unsigned *l,int *c)
+#define YY_DECL	    int yylex(Lval *lv, unsigned *l, Program *c)
 YY_DECL;
 
 // parser interface
