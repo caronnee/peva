f4ef3567ecaaa6240d4c2d75acec7f8947d14584
caronnee <caronnee@11875ef8-eda2-11dd-ada0-abde575de245>
2009-05-27 11:11:26 +0000

    nejake upravy, tree by malo KONECNE fungovat ok:)

--- trunk/language/tree/my_tree.cpp
+++ trunk/language/tree/my_tree.cpp
@@ -6,15 +6,15 @@ int main(int c, char** r)
 	Type t;
 	Program b;
 	std::cout << "Aplhabet:" << b.alphabet << std::endl;
-/*	std::string s[] = {"came", "car","cat", "cave", "cy", "cyan", "we", "went", "were", "west", "western"}; 
+	std::string s[] = {"came", "car","cat", "cave", "cy", "cyan", "we", "went", "were", "west", "western"}; 
 	std::string a = ".0123456789ABCDEFGHIJKLMNOPQRSTUVXYZ_abcdefghijklmnopqrstuvxyz";
 	for (int i =0; i< 11; i++)
 		b.add_string(s[i],t);
 	std::cout << "zacinam vypisovat:" << b.defined.depth << std::endl;
-	b.output(&b.defined);*/
-	for (int i =0; i< b.alphabet.length(); i++)
+	b.output(&b.defined);
+/*	for (int i =0; i< b.alphabet.length(); i++)
 	{
 		std::cout << b.find_index(b.alphabet[i]) << std::endl;
-	}
+	}*/
 	return 0;
 }
