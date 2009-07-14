cb6a57c0cf2180bca04f36b292da612cb7fd7429
caronnee <caronnee@11875ef8-eda2-11dd-ada0-abde575de245>
2009-07-14 15:09:32 +0000

    Trocha kompilovatelnej zmeny

--- trunk/language/parser_functions.h
+++ trunk/language/parser_functions.h
@@ -2,8 +2,9 @@
 #define __BISON_HELP__
 
 #include "./tree/my_stack.h"
-
-void add(Program * p, std::vector<std::string>, Type t);
+typedef std::vector<std::string> strings;
+void add_variables(Program * p, strings s, Create_type t);
+/*void add(Program * p, std::vector<std::string>, Type t);
 void add(Program * p, std::string s, Type t);
 void add(Program * p, std::string s, int value);
 void add(Program * p, std::string s, float value);
@@ -14,5 +15,5 @@ void set_element_type(Program *p, std::vector<Array *> arr, Type t);
 Node * add_array(Program *p, std::string s, std::vector<int> range, Type t);
 Node * add_array(Program *p, std::string s);
 void add_array(std::vector<Node *> n, Type t);
-
+*/
 #endif
