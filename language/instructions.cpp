779a78432b36e35664d461223a4bccfc75e9053e
caronnee <caronnee@11875ef8-eda2-11dd-ada0-abde575de245>
2009-07-25 07:38:11 +0000

    vyzera skompiovtelne

--- language/instructions.cpp
+++ language/instructions.cpp
@@ -3,130 +3,130 @@
 InstructionCreate::InstructionCreate()
 {
 	name_ = "InstructionCreate";
-}
+};
 InstructionLoad::InstructionLoad()
 {
 	name_ = "InstructionLoad";
-}
+};
 InstructionLoad::InstructionLoad(std::string s)
 {
 	name_ = "InstructionLoad";
-}
+};
 InstructionLoad::InstructionLoad(int i)
 {
 	name_ = "InstructionLoad";
-}
+};
 InstructionLoad::InstructionLoad(float f)
 {
 	name_ = "InstructionLoad";
-}
+};
 InstructionStore::InstructionStore()
 {
 	name_ = "InstructionStore";
-}
+};
 Call::Call()
 {
 	name_ = "Call";
-}
+};
 CallMethod::CallMethod()
 {
 	name_ = "CallMethod";
-}
+};
 CallMethod::CallMethod(std::string s)
 {
 	name_ = "CallMethod";
-}
+};
 InstructionPop::InstructionPop()
 {
 	name_ = "InstructionPop";
-}
+};
 InstructionMustJump::InstructionMustJump(int steps)
 {
 	name_ = "InstructionMustJump";
-}
+};
 InstructionJump::InstructionJump(int yes, int no)
 {
 	name_ = "InstructionJump";
-}
+};
 InstructionReturn::InstructionReturn()
 {
 	name_ = "InstructionReturn";
-}
+};
 InstructionBreak::InstructionBreak()
 {
 	name_ = "InstructionBreak";
-}
+};
 InstructionPlusPlus::InstructionPlusPlus()
 {
 	name_ = "InstructionPlusPlus";
-}
+};
 InstructionMinusMinus::InstructionMinusMinus()
 {
 	name_ = "InstructionMinusMinus";
-}
+};
 InstructionPlus::InstructionPlus()
 {
 	name_ = "InstructionPlus";
-}
+};
 InstructionMinus::InstructionMinus()
 {
 	name_ = "InstructionMinus";
-}
+};
 InstructionMultiply::InstructionMultiply()
 {
 	name_ = "InstructionMultiply";
-}
+};
 InstructionDivide::InstructionDivide()
 {
 	name_ = "InstructionDivide";
-}
+};
 InstructionModulo::InstructionModulo()
 {
 	name_ = "InstructionModulo";
-}
+};
 InstructionBinaryAnd::InstructionBinaryAnd()
 {
 	name_ = "InstructionBinaryAnd";
-}
+};
 InstructionBinaryOr::InstructionBinaryOr()
 {
 	name_ = "InstructionBinaryOr";
-}
+};
 //----------------------------------------------------------------------------------------------------
 InstructionGe::InstructionGe()
 {
 	name_ = "InstructionGe";
-}
+};
 InstructionGt::InstructionGt()
 {
 	name_ = "InstructionGt";
-}
+};
 InstructionEqual::InstructionEqual()
 {
 	name_ = "InstructionEqual";
-}
+};
 InstructionLt::InstructionLt()
 {
 	name_ = "InstructionLt";
-}
+};
 InstructionLe::InstructionLe()
 {
 	name_ = "InstructionLe";
-}
-InstructionBegin::InstructionBegin
+};
+InstructionBegin::InstructionBegin()
 {
 	name_ = "InstructionBegin";
-}
-InstructionEndBlock::InstructionEndBlock
+};
+InstructionEndBlock::InstructionEndBlock()
 {
 	name_ = "InstructionEndBlock";
-}
+};
 
 std::string Instruction::name()
 {
 	return name_;
-}
+};
 Instruction::Instruction()
 {
 	values = NULL;
-}
+};
