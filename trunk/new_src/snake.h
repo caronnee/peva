04cc617fb1ea09ac5f8233b06fef5f65ea18e052
caronnee <caronnee@11875ef8-eda2-11dd-ada0-abde575de245>
2009-08-01 14:48:26 +0000

    prerobeny a skompilovatelny snake

--- trunk/new_src/snake.h
+++ trunk/new_src/snake.h
@@ -1,4 +1,36 @@
 #ifndef ___SNAKE____
 #define ___SNAKE____
 
+#include "movement.h"
+#include "position.h"
+
+#define MOVEMENTS 4
+#define LAST_VISITED 4
+
+class Snake
+{
+	void Init();	
+	int fatness;
+	int health;
+public:
+	Snake(const Snake& a);
+	Snake(Position resolution_);
+
+	int max_interval;
+
+	int * visited; //zapamataj si predposledne 4 policka
+	Position resolution;
+	Position position;
+	Position direction;
+
+	Movement movements[MOVEMENTS]; //zoradenie zmeny oproti resolution		
+	bool move();
+
+	int get_x();
+	int get_y();
+	int get_fat();
+	int get_direct_x();
+	int get_direct_y();
+};
+
 #endif
