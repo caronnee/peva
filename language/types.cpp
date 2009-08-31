75a067a84a99f29a6a1c69b97aa202cf6426acc7
caronnee <caronnee@11875ef8-eda2-11dd-ada0-abde575de245>
2009-08-31 16:25:05 +0000

    upravene typy preporovnavanie creatur

--- language/types.cpp
+++ language/types.cpp
@@ -27,12 +27,58 @@ Create_type::Create_type(const Create_type & t)
 {
 	range = t.range;
 	data_type = NULL;
+	for(size_t i = 0; i<t.nested_vars.size(); i++)
+	{
+		Record r;
+		r.name = nested_vars[i].name;
+		r.type = new Create_type(*nested_vars[i].type);
+		nested_vars.push_back(r);
+	}
+	if (t.data_type!=NULL)
+	{
+		data_type = new Create_type(*t.data_type);
+	}
 	type = t.type;
 }
 
-Create_type& Create_type::composite ( Create_type t)
+Create_type& Create_type::composite ( Create_type t) //vrati sa novy typ, Location = type.add(TYPE_INTEGER, INT)
 {
 	Create_type comp(*this);
-	comp.data_type = &t; //TODO seriozny BUG! priradzujem typ, co za chvilinku zanikne
+	comp.data_type = new Create_type(t); //TODO mozo obmedzit hlbku na 20?
 	return *this;
+}
+void Create_type::add(std::string name, Create_type t)
+{
+	Record r;
+	r.name = name;
+	r.type = new Create_type(t);
+	nested_vars.push_back(r);
+}
+Create_type::~Create_type()
+{
+	if (data_type!=NULL)
+		delete data_type;
+}
+bool Create_type::operator!=(const Create_type& t)
+{
+	return !((*this)==t);
+}
+bool Create_type::operator==(const Create_type &t)
+{
+	if (t.type!=type)
+		return true;
+	if (nested_vars.size()!=t.nested_vars.size())
+		return false;
+	for(size_t i =0; i<nested_vars.size(); i++)
+	{
+		if (nested_vars[i].name!=t.nested_vars[i].name)
+			return false;
+		if ((*nested_vars[i].type) !=(*t.nested_vars[i].type))
+			return false;
+	}
+	if (range!=t.range)
+		return false;
+	if ((*data_type)!=(*t.data_type))
+		return false;
+	return true;
 }
--- language/types.h
+++ language/types.h
@@ -39,12 +39,16 @@ struct Create_type
 {
 	int range;
 	Create_type * data_type;
-	std::vector<Record> nested_vars;
+	std::vector<Record> nested_vars; //pre strukty
 	Type type;
 	Create_type();
 	Create_type(Type t, int rang = 0);
 	Create_type(const Create_type& t);
 	Create_type & composite (Create_type t);
+	void add(std::string name, Create_type t);
+	bool operator==(const Create_type& t);
+	bool operator!=(const Create_type& t);
+	~Create_type();
 };
 
 #endif
