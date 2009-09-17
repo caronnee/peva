865e6bc7d32ad97b3a7a9d8f2127bf5c5c1bd2ce
caronnee <caronnee@11875ef8-eda2-11dd-ada0-abde575de245>
2009-09-17 23:30:15 +0000

    upravena pamat

--- trunk/language/memory.h
+++ trunk/language/memory.h
@@ -4,24 +4,59 @@
 #include <stack>
 #include <ctime>
 #include <cstdlib>
+#include <vector>
 #include "variable.h"
 
 struct Memory_record
 {
-	size_t owner, depth;
+	size_t depth;
 	Variable * variable;
-	size_t id;
 };
-struct Memory
+
+class Memory
 {
+	/* Position where to begint o check free space */
+	size_t position;
+
+	/* number of units that is possible to allocate */
 	int memory_size;
-	std::stack<int> id_free_vars;
+
+	/* structure to hold assigned variables */
 	std::vector<Memory_record> assigned;
+
+	/* structure to hold temporarily assigned variables */
+	std::stack<Variable *> temp;
+
+	/* memory structure */
 	Variable ** memory;
-	void free(size_t depth);
-	Variable * assign(Create_type t,size_t id, size_t siz);
+
+	/* returns nearest possible free variable */
+	Variable * next_id(size_t ID);
+
+	/* returns variable with Type t */
+	Variable * find_free(Create_type t, size_t ID);
+
+	/* sets variable v and structures in it as free */
+	void set_free(Variable * v);
+
+public:
+	/* class constructor */
 	Memory(int size = 100);
-	void realok(int size);
+
+	/* returns variable added in depth d */
+	Variable * assign(Create_type t, size_t ID,size_t depth);
+
+	/* returns variable  temporarily added */
+	Variable * assign_temp(Create_type t);
+
+	/* frees occupied memory with depth greater or equal to depth */
+	void free(size_t depth);
+
+	/* frees temporary variable */
+	void free_tmp();
+
+	/* reallocatin memory to new size */
+	void realloc(int size);
 };
 
 #endif
