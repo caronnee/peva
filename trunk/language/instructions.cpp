a46849746c6cd764b5e64ef5edc899f68274e15c
caronnee <caronnee@11875ef8-eda2-11dd-ada0-abde575de245>
2009-10-29 00:04:19 +0000

    kontrola array

--- trunk/language/instructions.cpp
+++ trunk/language/instructions.cpp
@@ -1,4 +1,5 @@
-//TODO namieto removetemp = opreacia +remove temp, aby to bolo atomicke
+//TODO namiesto removetemp = opreacia +remove temp, aby to bolo atomicke
+//TODO return value by sa mala vytvorit az v okamziku RETURN u
 #include "instructions.h"
 #include <iostream>
 
--- trunk/language/variable.cpp
+++ trunk/language/variable.cpp
@@ -23,7 +23,7 @@ void Variable::copyValue(Variable *v) //akopiruje aj neinicializovane hodnoty, m
 	realValue = v->realValue;
 	objectValue = v->objectValue;
 	//este sa postarat o arraye
-	std::cout << "element size parametrer:" << v->array.elements.size(); getc(stdin);
+//	std::cout << "element size parametrer:" << v->array.elements.size(); getc(stdin);
 
 //	std::cout << "size function parameter-" << array.elements[0]; getc(stdin);
 	std::vector<Variable *> variables_to_copy;
