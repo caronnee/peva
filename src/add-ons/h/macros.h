02d909184db11ac10f3c60886aa5e8fddab335bc
caronnee <caronnee@11875ef8-eda2-11dd-ada0-abde575de245>
2010-05-17 14:51:33 +0000

    zmenene mapy pre vysiu visitelnost

--- src/add-ons/h/macros.h
+++ src/add-ons/h/macros.h
@@ -2,7 +2,7 @@
 #ifdef DEBUG
 #define TEST(x) { std::cout << x ; getc(stdin); }
 #else 
-#define TEST(x) {}
+#define TEST(x) { std::cout << x << std::endl;}
 #endif
 
 #define List std::list<Object *>
