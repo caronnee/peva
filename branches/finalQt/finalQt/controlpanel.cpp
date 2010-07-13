badf95dd17401ea0843380aedb36e01abf51f72f
caronnee <caronnee@11875ef8-eda2-11dd-ada0-abde575de245>
2010-07-13 17:54:33 +0000

    control annel je scrollable..jake to vitazsvo!;)

--- branches/finalQt/finalQt/controlpanel.cpp
+++ branches/finalQt/finalQt/controlpanel.cpp
@@ -2,7 +2,6 @@
 #include <iostream>
 #include <QHBoxLayout>
 #include <QLabel>
-#include <QScrollArea>
 
 controlPanel::controlPanel(QWidget *parent, Qt::WFlags flags)
 	: QMainWindow(parent, flags)
@@ -11,41 +10,30 @@ controlPanel::controlPanel(QWidget *parent, Qt::WFlags flags)
 	this->insWin = new QMainWindow(this);
 	ins.setupUi(this->insWin);
 
-	//this->insWin->show();
-	//QObject::connect(this->ui.mapButton, SIGNAL(clicked()), &b, SLOT(close()));
-//	new QPushButton("ddd",this->insWin);
-//	new QPushButton("ddd2",this->insWin);
-	QScrollArea * scr = new QScrollArea;
-	myCentral = new QWidget;
-	scr->setWidget(myCentral);
-	this->insWin->setCentralWidget( scr );
+	QWidget * myCentral = new QWidget(this->insWin);
+	QVBoxLayout * vLayout = new QVBoxLayout(myCentral);
+	QScrollArea * scrollArea = new QScrollArea(myCentral);
+	vLayout->addWidget(scrollArea);      
+       
+      //set up parameters for scroll area
+	scrollArea->setWidgetResizable(false);      
+       
+      //create another widget with a QVBoxLayout
+	QVBoxLayout * scrollAreaVLayout = new QVBoxLayout();
+	scrollAreaWidgetContents = new QWidget(); 
+	scrollAreaWidgetContents->setLayout(scrollAreaVLayout);
+	scrollAreaVLayout->setSizeConstraint(QLayout::SetFixedSize);  
+       
+      //add scrolling widget to scroller
+	scrollArea->setWidget(scrollAreaWidgetContents);      
+	this->insWin->setCentralWidget(myCentral);
 
-	QVBoxLayout * l = new QVBoxLayout;
-	l->addWidget(new QLabel("ttt"));
-	l->addWidget(new QLabel("ttt2"));
-	l->addWidget(new QLabel("ttt3"));
-
-	myCentral->setLayout(l);
-	QObject::connect(this->ui.InstrructionButton, SIGNAL(clicked()), this->insWin, SLOT(show()));
 	QObject::connect(this->ui.mapButton, SIGNAL(clicked()), this, SLOT(mySet()));
+	insWin->show();
 }
 
 void controlPanel::mySet()
 {
-	//zistit, ci sa layout netreba nahodou deinizializovat
-	std::cout << "test";
-
-	QHBoxLayout * l = new QHBoxLayout;
-	l->addWidget(new QLabel("ttt"));
-	l->addWidget(new QLabel("ttt2"));
-	l->addWidget(new QLabel("ttt3"));
-	myCentral->layout()->addItem(l);
-	new QPushButton("d",this->insWin);
-	myCentral->layout()->addWidget(new QPushButton("d1",this->myCentral));
-	myCentral->setLayout(myCentral->layout());
-	insWin->setCentralWidget(myCentral);
+	scrollAreaWidgetContents->layout()->addWidget(new QPushButton("test", scrollAreaWidgetContents));
 }
-controlPanel::~controlPanel()
-{
-
-}
+controlPanel::~controlPanel() { }
