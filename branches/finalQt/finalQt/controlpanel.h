77f55e9c9443516fbc598c5ed0cd58d80f86b5a1
caronnee <caronnee@11875ef8-eda2-11dd-ada0-abde575de245>
2010-07-27 22:52:24 +0000

    zobrazenie vyberu map OK

--- branches/finalQt/finalQt/controlpanel.h
+++ branches/finalQt/finalQt/controlpanel.h
@@ -3,16 +3,24 @@
 
 #include <vector>
 #include <QtGui/QMainWindow>
+#include <QtGui/QDialog>
 #include "ui_controlpanel.h"
 #include <QScrollArea>
 #include "ui_instructions.h"
+#include "ui_maps.h"
+#include <list>
+#include <QFile>
+#include <QDir>
 
 class controlPanel : public QMainWindow
 {
 	Q_OBJECT
 
-public slots:
+public slots:;
 	void mySet();
+	void choose();
+	void unchoose();
+	void changeDir(QListWidgetItem *itm);
 
 public:
 	controlPanel(QWidget *parent = 0, Qt::WFlags flags = 0);
@@ -23,7 +31,11 @@ private:
 	QWidget * scrollAreaWidgetContents;
 	QMainWindow * insWin;
 	Ui::controlPanelClass ui;
+	QDialog * mapWin;
 	Ui::Instruction ins;
+
+	Ui::Dialog dialog;
+	QDir directory;
 };
 
 #endif // CONTROLPANEL_H
